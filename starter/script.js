08f116348d768018448d5bd15202c65ad6138fd4
Ramil Mamedov <74110914+ramilmamedoff@users.noreply.github.com>
2021-01-03 17:32:02 +0400

    Add files via upload

---
+++ starter/script.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// Show input error message
+function showError() {}
+
+// Show success outline
+function showSuccess() {}
+
+// Check email is valid
+function checkEmail() {}
+
+// Check required fields
+function checkRequired() {}
+
+// Check input length
+function checkLength() {}
+
+// Check passwords match
+function checkPasswordsMatch() {}
